14f1607fae4db58c1f23da59623a69f59ef49b4a
ConfidenceYobo <confiyobo@gmail.com>
2024-06-10 17:16:52 +0100

    add web3 functionality

---
+++ src/constants/contracts.ts
@@ -0,0 +1,10 @@
+import { ChainId } from "@/types";
+
+const contractAddresses = {
+  nft: {
+    [ChainId.TESTNET]: "0xc659e4Ba11D355B99D6a768a5492Fae8A789ac3F",
+    [ChainId.MAINNET]: "",
+  },
+};
+
+export default contractAddresses;
